ff5cff07d2d7dc0f9aa3566b3a847e1143528bf8
TradsonChai <275753675@qq.com>
2017-01-13 16:36:07 +0800

    no message

--- code/app/src/main/java/me/twobirds/animationexer/activity/MainActivity.java
+++ code/app/src/main/java/me/twobirds/animationexer/activity/MainActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import me.twobirds.animationexer.R;
@@ -14,15 +16,15 @@ import me.twobirds.animationexer.R;
 public class MainActivity extends AppCompatActivity {
 
 
+    @BindView(R.id.btn_base_anim)
+    Button baseAnimBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("cth","before ButterKnife.bind");
         ButterKnife.bind(this);
-        Log.i("cth","after ButterKnife.bind");
-
 
     }
 
